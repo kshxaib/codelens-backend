c1e20611633e634ea2b533dd83c5d32567a28cfe
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2026-08-17 18:49:05 +0530

    Improving GitHub service

--- app/github/service.py
+++ app/github/service.py
@@ -13,16 +13,51 @@ class GitHubService:
             "X-GitHub-Api-Version": "2026-03-10",
         }
 
+    # fetch repositories of the user from github
     async def get_repositories(self):
+        repositories = []
+        page = 1
+
         async with httpx.AsyncClient() as client:
-            response = await client.get(f"{GITHUB_API_URL}/user/repos",
-                headers=self._headers(),
-                params={
-                    "per_page": 100,
-                    "sort": "updated",
-                }
+
+            while True:
+                response = await client.get(
+                    f"{GITHUB_API_URL}/user/repos",
+                    headers=self._headers(),
+                    params={
+                        "per_page": 100,
+                        "page": page,
+                        "visibility": "all",
+                        "affiliation": (
+                            "owner,"
+                            "collaborator,"
+                            "organization_member"
+                        ),
+                        "sort": "updated",
+                    }
+                )
+
+                response.raise_for_status()
+                data = response.json()
+
+                if not data:
+                    break
+
+                repositories.extend(data)
+
+                if len(data) < 100:
+                    break
+
+                page += 1
+
+        return repositories
+
+    # fetch repository details from github
+    async def get_repository(self, owner: str, name: str):
+        async with httpx.AsyncClient() as client:
+            response = await client.get(
+                f"{GITHUB_API_URL}/repos/{owner}/{name}",
+                headers=self._headers()
             )
 
-        response.raise_for_status()
-
-        return response.json()
+        return response
